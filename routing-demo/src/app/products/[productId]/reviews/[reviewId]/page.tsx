77b60ff53241546eac4f79dc7b263ab9f65e8a0a
Suzy-Lee <>
2024-03-14 20:46:49 +0900

    study: 8 - Nested Dynamic Routes
    #7
    http://localhost:3000/products/5/reviews/3
    대괄호 폴더가 두번인 경우 구조

---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,16 @@
+function ReviewDetail({
+  params,
+}: {
+  params: {
+    productId: string;
+    reviewId: string
+  };
+}) {
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
+}
+
+export default ReviewDetail;
