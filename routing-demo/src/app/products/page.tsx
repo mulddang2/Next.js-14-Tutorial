24a44e7f05ffc3e556953e4c04236ec4c731e801
Suzy-Lee <>
2024-03-20 17:13:37 +0900

    study: 19 - Link Component Navigation
    #18

--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,27 @@
+import Link from 'next/link';
+
 export default function ProductList() {
+  const productId = 100;
   return (
     <>
+      <Link href={'/'}>홈으로 돌아가기</Link>
       <h1>Product List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <h2>
+        <Link href={'products/1'}>Product 1</Link>
+      </h2>
+      <h2>
+        <Link href={'products/2'}>Product 2</Link>
+      </h2>
+      <h2>
+        <Link href='products/3' replace>
+          {/* replace 속성을 사용할 경우, 뒤로가기 하면, 홈으로 돌아감. */}
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        {/* 동적으로 링크 연결하기 */}
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
-  )
+  );
 }
